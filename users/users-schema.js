799fad465c7fe2d8f39f9f43083e40e79272927d
lukagolf <jovanovic.l@northeastern.edu>
2023-06-21 19:13:10 -0400

    Multirole login & registration

--- users/users-schema.js
+++ users/users-schema.js
@@ -1,4 +1,27 @@
 import mongoose from "mongoose";
+
+// Custom validator for roles
+function roleValidator(value) {
+    if (value.length > 2) return false; // can't have more than two roles
+
+    // Define valid combinations
+    const validCombinations = [["ADMIN", "CRITIC"], ["ADMIN", "VIEWER"]];
+
+    // If the user only has one role, it is always valid
+    if (value.length === 1) return true;
+
+    // If user has two roles, check if the combination is valid
+    const sortedRoles = [...value].sort();
+    for (let combo of validCombinations) {
+        if (JSON.stringify(combo.sort()) === JSON.stringify(sortedRoles)) {
+            return true;
+        }
+    }
+
+    // If it gets here, the combination is invalid
+    return false;
+}
+
 const usersSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
@@ -6,6 +29,12 @@ const usersSchema = new mongoose.Schema({
     lastName: String,
     email: String,
     followedCritics: Array,
-    role: { type: String, enum: ["ADMIN", "CRITIC", "VIEWER"], default: "VIEWER" },
+    roles: {
+        type: [String],
+        enum: ["ADMIN", "CRITIC", "VIEWER"],
+        validate: [roleValidator, 'Invalid role combination.'],
+        default: ["VIEWER"]
+    },
 }, { collection: "users" });
+
 export default usersSchema;
